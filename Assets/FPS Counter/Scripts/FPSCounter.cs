2636dd920669cd7192dae82586a1226a6c5a2c5c
JasonSkillman <jason_skillman@outlook.com>
2019-03-22 15:33:38 -0400

    Added DontDestroyOnLoad()

--- Assets/FPS Counter/Scripts/FPSCounter.cs
+++ Assets/FPS Counter/Scripts/FPSCounter.cs
@@ -11,7 +11,11 @@ public class FPSCounter : MonoBehaviour {
 	private float deltaTime = 0.0f;
 
 
-	public void Update() {
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Update() {
 		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
